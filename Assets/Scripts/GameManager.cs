4fe72b0edc07e651cf37df1b6e120c685a5915ef
Anré "Ray" Tanner <40047765+A-Tanner@users.noreply.github.com>
2024-08-18 17:24:57 -0700

    Add timer, more sensible player speed (#25)

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -8,6 +8,8 @@ public class GameManager : MonoBehaviour
 {
     [SerializeField]
     private GameObject _playerPrefab;
+    private float _timeAliveInSeconds = 0;
+    private bool _isPlaying = true;
 
     private void Awake()
     {
@@ -16,10 +18,19 @@ public class GameManager : MonoBehaviour
         Debug.Log(PlayerScript.OnDeathEvent);
     }
 
+    private void Update()
+    {
+       if (_isPlaying)
+        {
+            _timeAliveInSeconds += Time.deltaTime;
+        }
+    }
+
     public void GameOver()
     {
         PlayerScript.OnDeathEvent -= GameOver;
         SceneManager.LoadScene("Results");
+        _isPlaying = false;
         GameStartButton.OnStartEvent += StartGame;
     }
 
@@ -29,5 +40,10 @@ public class GameManager : MonoBehaviour
         SceneManager.LoadScene("Gameplay");
         Instantiate(_playerPrefab, new Vector3(0, 5, 0), Quaternion.identity);
         Destroy(gameObject);
+    }
+
+    public float GetTimeAlive()
+    {
+        return _timeAliveInSeconds;
     }
 }
