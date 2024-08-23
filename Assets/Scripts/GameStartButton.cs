f2a77d3f62f277ebea762a5e1e164a3702bfdc01
shweshipu <shwesh2>
2024-08-22 23:09:15 -0700

    add functionality to restart the game using spacebar

--- Assets/Scripts/GameStartButton.cs
+++ Assets/Scripts/GameStartButton.cs
@@ -7,9 +7,24 @@ public class GameStartButton : MonoBehaviour
 {
     public delegate void OnStart();
     public static OnStart OnStartEvent;
+    private float _waitTime = 0.3f;
 
     public static void StartAction()
     {
         OnStartEvent.Invoke();
+    }
+    void Update()
+    {
+        
+        if(_waitTime > 0)
+        {
+            _waitTime -= Time.deltaTime;
+            return;
+        }
+
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            StartAction();
+        }
     }
 }
