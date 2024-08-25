3043b928b668a094c0c5e144300a905fec855c99
shweshipu <shwesh2>
2024-08-24 20:42:37 -0700

    fix rotation speed

--- Assets/Scripts/Rotate.cs
+++ Assets/Scripts/Rotate.cs
@@ -5,13 +5,14 @@ using UnityEngine;
 public class Rotate : MonoBehaviour
 {
     private float _rotateSpeed = 0;
+    private float _scale = 60;
     private void Awake()
     {
-        _rotateSpeed = (Random.value* 3f)- 1.5f;
+        _rotateSpeed = (Random.value* 3f * _scale)- 1.5f * _scale;
         transform.Rotate(0, Random.value*180, 0);
     }
     private void Update()
     {
-        transform.Rotate(0, _rotateSpeed, 0);
+        transform.Rotate(0, _rotateSpeed * Time.deltaTime, 0);
     }
 }
